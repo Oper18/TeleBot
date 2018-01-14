780bc1414b147a8886b44bdc2275c7e6e55800e1
oper <genovikov93@gmail.com>
2018-01-14 23:38:43 +0300

    Create buttons

--- bot.py
+++ bot.py
@@ -1,25 +1,25 @@
 # -*- coding: utf-8 -*-
 
 import config
-import telebot, eventlet, logging, requests, types
+import datetime
+import telebot, eventlet, logging, requests
+from telebot import TeleBot, types
 
 bot = telebot.TeleBot(config.telegramToken)
 
 HelloText = 'Приветствую тебя! Зарегестрируйся для возможности просмотра заказов. ' \
             'Другие участники иогут видеть специальность и контакты только зарегистрированных пользователей.'
 
-@bot.message_handler(func = lambda message: True, content_types=["text"])
+# @bot.message_handler(func = lambda message: True, content_types=["start"])
+@bot.message_handler(commands=["start"])
 def sendMessage(message):
-    # bot.send_message(message.chat.id, 'Heil')
-    # bot.send_message('@testapibotchan', 'Heil')
-    # if message.text == '/heil':
-    #     bot.send_message(message.chat.id, 'Heil')
-    bot.send_message(message.chat.id, HelloText)
-
-def StartList(message):
-    keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
-    FirstKey = types.KeyboardButton(text="1st Button", request_contact=True)
-    keyboard.add(FirstKey)
+    # bot.send_message(message.chat.id, HelloText)
+    keyboard = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
+    TaskBut = types.KeyboardButton(text='Задачи')
+    UserBut = types.KeyboardButton(text='Список пользователей')
+    SpecBut = types.KeyboardButton(text='Специализация')
+    keyboard.add(TaskBut, UserBut, SpecBut)
+    bot.send_message(message.chat.id, HelloText, reply_markup=keyboard)
 
 if __name__ == '__main__':
     bot.polling(none_stop = True)
